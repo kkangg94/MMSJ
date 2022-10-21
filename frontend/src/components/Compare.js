9aca2bd177a64d8904d99e9b43e6177d4876a0fe
Heo-won <106522988+Heo-won@users.noreply.github.com>
2022-10-22 02:12:21 +0900

    git p

--- frontend/src/components/Compare.js
+++ frontend/src/components/Compare.js
@@ -7,9 +7,15 @@ function Compare() {
       <header className="header">
         <div className="brand">
           <button onclick="openMenu()">&#9776;</button>
-          <a href="index.html">amazona</a>
+          <a href="index.html">뭐마시지?</a>
         </div>
-        <div className="header-links">
+        <div className="header-left-links">
+          <a href="cart.html">메뉴</a>
+          <a href="signin.html">소식</a>
+          <a href="board.html">자유게시판</a>
+          <a href="help.html">고객센터</a>
+        </div>
+        <div className="header-right-links">
           <a href="cart.html">Cart</a>
           <a href="signin.html">Sign In</a>
         </div>
@@ -38,7 +44,13 @@ function Compare() {
           </ul>
         </div>
       </main>
-      <footer className="footer">All right reserved.</footer>
+      <footer className="footer">
+        <p>
+          사업자등록번호 : 201-81-21515 주식회사 에스씨케이컴퍼니 대표이사 : 송
+          데이비드 호섭 TEL : 1522-3232 개인정보 책임자 : 이현수
+        </p>
+        <p>All right reserved.</p>
+      </footer>
     </>
   );
 }
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,30 +1,22 @@
-import React from 'react'
-import Rating from './Rating';
-import data from '../data';
-
+import React from "react";
+import Rating from "./Rating";
+import data from "../data";
 
 export default function Product(props) {
-    const { product } = props;
+  const { product } = props;
   return (
-<>
-<li>
-    <div key={product._id} className="product">
-      <img
-        className="product-image"
-        src="/images/p1.png"
-        alt="product"
-      />
-      <div className="product-name">
-        <a href="product.html">{product.name}</a>
-      </div>
-      <div className="product-brand">{product.brand}</div>
-  <Rating rating={product.rating} numReviews={product.numReviews} /> 
-      <div className="product-price">{product.price}</div>
-
-      
-    </div>
-  </li>
-</>
-   
-  )
+    <>
+      <li>
+        <div key={product._id} className="product">
+          <img className="product-image" src={product.image} alt="product" />
+          <div className="product-name">
+            <a href="product.html">{product.name}</a>
+          </div>
+          <div className="product-brand">{product.brand}</div>
+          <div className="product-price">{product.price}</div>
+          <Rating rating={product.rating} numReviews={product.numReviews} />
+        </div>
+      </li>
+    </>
+  );
 }
