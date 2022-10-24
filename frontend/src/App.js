7720f5747a9e0498e627192f8d8612856cbf61d1
1 <106522988+Heo-won@users.noreply.github.com>
2022-10-24 20:21:06 +0900

    won

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import Compare from "./components/Compare";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-    <Compare />
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/product/:id" element={<ProductScreen />}></Route>
+          <Route path="/" element={<HomeScreen />} exact></Route>
+        </Routes>
+      </BrowserRouter>
+      <Compare />
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <p>
+          사업자등록번호 : 201-81-21515 주식회사 에스씨케이컴퍼니 대표이사 : 송
+          데이비드 호섭 TEL : 1522-3232 개인정보 책임자 : 이현수
+        </p>
+        <p>All right reserved.</p>
+      </footer>
+    </div>
+  );
+}
