8e57ddf0762ef1100b5d62c7b2b64e3c8072867a
kanngji <50470748+kanngji@users.noreply.github.com>
2022-10-26 18:21:52 +0900

    jh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
       </Routes>
 
       {/* <Compare /> */}
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- frontend/src/screens/BoardScreen.js
+++ frontend/src/screens/BoardScreen.js
@@ -1,5 +1,29 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import "./BoardScreen.css";
+import Header from "../components/Header";
+import BoardDetail from "../components/BoardDetails";
 
 export default function BoardScreen() {
-  return <>asdasds</>;
+  const [boards, setBoards] = useState(null);
+  useEffect(() => {
+    const fetchBoards = async () => {
+      const response = await fetch("/api/boards");
+      const json = await response.json();
+
+      if (response.ok) {
+        setBoards(json);
+      }
+    };
+    fetchBoards();
+  }, []);
+
+  return (
+    <div className="boardScreen">
+      {/* <Header></Header> */}
+      <div className="boards">
+        {boards &&
+          boards.map((board) => <BoardDetail key={board._id} board={board} />)}
+      </div>
+    </div>
+  );
 }
