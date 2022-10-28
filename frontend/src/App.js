9aa90aa0cd27f2f06f272a41fa824f5a765b7360
kanngji <50470748+kanngji@users.noreply.github.com>
2022-10-28 16:13:45 +0900

    404

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,23 +15,26 @@ import MainPage from "./screens/MainPage";
 import BoardScreen from "./screens/BoardScreen";
 import Signup from "./screens/Signup";
 import Login from "./screens/Login";
+import Notfound from "./components/Notfound";
 
 function App() {
   return (
     <>
       {/* <Compare /> */}
       {/* <Map></Map> */}
+      {/* <Notfound></Notfound> */}
 
       <Routes>
         <Route
           path="/api/compare/product/:id"
           element={<ProductScreen />}
         ></Route>
-        <Route path="/api/compare" element={<HomeScreen />} exact></Route>
         <Route path="/" element={<MainPage />} exact></Route>
+        <Route path="/api/compare" element={<HomeScreen />} exact></Route>
         <Route path="/api/board" element={<BoardScreen />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
+        <Route path="*" element={<Notfound />}></Route>
       </Routes>
 
       {/* <Compare /> */}
